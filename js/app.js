af8df4634e43d50c4c42bfd20ca1d8052a786f27
kherma <kamil.herma01@gmail.com>
2021-01-07 00:26:11 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 import Portfolio from './Components/Portfolio.js';
 import Challenge from './Components/Challenges.js';
+import Fcc from './Components/Fcc.js';
 
 const app = {
   initApp() {
     Portfolio.generatePortfolio();
     Challenge.generateChallenge();
+    Fcc.generateFcc();
   },
 };
 app.initApp();
